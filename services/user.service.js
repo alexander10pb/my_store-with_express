8f5936d87b77210ca633c00f7bb184f113072537
alexander10pb <alexanderpb010@gmail.com>
2023-11-13 01:13:18 -0500

    [add] manejando un pool de conexiones

--- services/user.service.js
+++ services/user.service.js
@@ -1,17 +1,22 @@
 const boom = require('@hapi/boom');
 
-const getConnection = require('../libs/postgres');
+// const getConnection = require('../libs/postgres');
+const pool = require('../libs/postgres.pool');
+const { query } = require('express');
 
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool= pool;
+    this.pool.on('error', (err) => console.error(err));
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const client = await getConnection();
-    const rta = await client.query('SELECT * FROM tasks');
+    const query = 'SELECT * FROM tasks'
+    const rta = await pool.query(query);
     return rta.rows;
   }
 
