904efe644f056b03b74edfa00c304409c006977a
alexander10pb <alexanderpb010@gmail.com>
2023-11-14 23:05:39 -0500

    [add]creacion de modelo de customer y su service, router y schema

--- db/models/index.js
+++ db/models/index.js
@@ -1,8 +1,10 @@
 const { User, UserSchema } = require('./user.model');
+const { Customer, CustomerSchema } = require('./customer.model')
 
 //Aqui se cargaran todos los modelos
 function setupModels(sequelize) {
     User.init(UserSchema, User.config(sequelize));
+    Customer.init(CustomerSchema, Customer.config(sequelize));
 }
 
 module.exports = setupModels;
