a541be55d92317c2331156f80d1012a42768bce4
alexander10pb <alexanderpb010@gmail.com>
2023-11-14 23:45:40 -0500

    [add] creacion de la tabla customers con migraciones, agregamos la FK, servicio de create customers, yagregando el router de customers.router.js

--- db/models/index.js
+++ db/models/index.js
@@ -5,6 +5,8 @@ const { Customer, CustomerSchema } = require('./customer.model')
 function setupModels(sequelize) {
     User.init(UserSchema, User.config(sequelize));
     Customer.init(CustomerSchema, Customer.config(sequelize));
+
+    Customer.associate(sequelize.models);
 }
 
 module.exports = setupModels;
