e2eaa37496c29944ca0e2fcb24e279edb1006de9
alexander10pb <alexanderpb010@gmail.com>
2023-11-16 00:35:47 -0500

    [add]agregando description y categoryId por la relacion, modificando logica del service

--- services/product.service.js
+++ services/product.service.js
@@ -1,7 +1,7 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom')
 
-const sequelize = require('../libs/sequilize');
+const { models } = require('../libs/sequilize');
 
 class ProductService {
 
@@ -24,24 +24,15 @@ class ProductService {
     }
 
     async create(data) {
-        const newProduct = {
-            id: faker.datatype.uuid(),
-            ...data
-        }
-        this.products.push(newProduct);
+        const newProduct = await models.Product.create(data);
         return newProduct;
     }
 
     async find() {
-        // const query = 'SELECT * FROM tasks';
-        // const [data, metadata] = await sequelize.query(query);
-        // return {
-        //     data,
-        //     metadata
-        // };
-        const query = 'SELECT * FROM tasks';
-        const [data] = await sequelize.query(query);
-        return data;
+        const products = await models.Product.findAll({
+            include: ['category']
+        });
+        return products;
     }
 
     async findOne(id) {
