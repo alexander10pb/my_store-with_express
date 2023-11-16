fe9831c326345526023e3ae5d2e256ff159e68e8
alexander10pb <alexanderpb010@gmail.com>
2023-11-15 23:08:04 -0500

    [add] resolviendo relaciones one to one, cambios en el user_id para que sea unico, que en ambas tablas nos traiga la informacion completa de sus FK y desde el sevicio de customer se puede crear el usuario enseguida

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -35,8 +35,11 @@ const UserSchema = {
 
 class User extends Model {
     // Metodos estaticos
-    static associate(){
-
+    static associate(models){
+        this.hasOne(models.Customer, {
+            as: 'customer',
+            foreignKey: 'userId'
+        })
     }
 
     static config(sequelize){
