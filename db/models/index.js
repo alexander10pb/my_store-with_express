4bfb34c76559cf5ef6356af1fc3e03b3aa275a12
alexander10pb <alexanderpb010@gmail.com>
2023-11-16 22:40:08 -0500

    [add]creacion de tabla order-product pivote y relaciones one to many

--- db/models/index.js
+++ db/models/index.js
@@ -3,6 +3,7 @@ const { Customer, CustomerSchema } = require('./customer.model');
 const { Category, CategorySchema } = require('./category.model');
 const { Product, ProductSchema } = require('./product.model');
 const { Order, OrderSchema } = require('./order.model');
+const { OrderProduct, OrderProductSchema } = require('./order-product.model')
 
 //Aqui se cargaran todos los modelos
 function setupModels(sequelize) {
@@ -11,6 +12,7 @@ function setupModels(sequelize) {
     Category.init(CategorySchema, Category.config(sequelize));
     Product.init(ProductSchema, Product.config(sequelize));
     Order.init(OrderSchema, Order.config(sequelize));
+    OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize));
 
     // Relaciones
     User.associate(sequelize.models);
@@ -18,6 +20,7 @@ function setupModels(sequelize) {
     Category.associate(sequelize.models);
     Product.associate(sequelize.models);
     Order.associate(sequelize.models);
+    OrderProduct.associate(sequelize.models);
 }
 
 module.exports = setupModels;
